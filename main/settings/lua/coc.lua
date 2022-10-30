3b95a5deb0851c9462764dc3e4a596f89f5e92bf
leoatchina <leoatchina@outlook.com>
2022-10-30 22:25:51 +0800

    update

--- main/settings/lua/coc.lua
+++ main/settings/lua/coc.lua
@@ -1,18 +1,9 @@
-local function symbol_line()
-  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
-  local ok, line = pcall(vim.api.nvim_buf_get_var, bufnr, 'coc_symbol_line')
-  return ok and line or ''
-end
-
 if vim.fn.exists '&winbar' then
   vim.api.nvim_create_autocmd( { 'CursorMoved', 'WinEnter', 'BufWinEnter' }, {
     pattern = '*',
     callback = function()
-      if vim.b.coc_symbol_line and vim.bo.buftype == '' then
-        if vim.opt_local.winbar:get() == '' then
-          local winvar = symbol_line()
-          vim.wo.winbar = winvar
-        end
+      if vim.b.vista_nearest_method_or_function and vim.bo.buftype == '' and vim.opt_local.winbar:get() == '' then
+          vim.wo.winbar = vim.b.vista_nearest_method_or_function
       else
         vim.wo.winbar = ''
       end
