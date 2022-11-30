565df1ed3d2a0c4a506f4cb8c76920db37b9c442
leoatchina <leoatchina@gmail.com>
2022-11-30 17:04:45 +0800

    update treesitter parse install dir

--- main/settings/lua/treesitter.lua
+++ main/settings/lua/treesitter.lua
@@ -1,4 +1,10 @@
 local map = vim.api.nvim_set_keymap
+local parser_install_dir = ''
+if UNIX() then
+  parser_install_dir = vim.fn.expand("~/.local/share/nvim/parsers")
+else
+  parser_install_dir = vim.fn.expand("~/AppData/Local/nvim-data/parsers")
+end
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = vim.g.highlight_filetypes,
   sync_install = true,
@@ -24,8 +30,11 @@ require 'nvim-treesitter.configs'.setup {
   },
   incrcmental_selection = {
     enable = false,
-  }
+  },
+  parser_install_dir = parser_install_dir
 }
+vim.opt.runtimepath:append(parser_install_dir)
+
 require 'nvim-treesitter.install'.prefer_git = true
 if Installed('nvim-treehopper') then
   map('o', '<C-s>', [[<cmd>lua require('tsht').nodes()<CR>]], { noremap = true, silent = true })
