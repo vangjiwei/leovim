f261db957593703c841f5e62cac9fe0add01fc89
leoatchina <leoatchina@outlook.com>
2023-01-24 17:21:45 +0800

    update

--- main/config/lua/lsp.lua
+++ main/config/lua/lsp.lua
@@ -46,8 +46,8 @@ end
 map('n', '<M-M>', [[<cmd>Mason<CR>]], opts)
 -- Telescope symbols
 map('n', '<leader>t', [[<cmd>Telescope lsp_document_symbols<CR>]], opts)
-map('n', 'f<Cr>',     [[<cmd>Telescope lsp_document_symbols symbols=function,class<CR>]], opts)
-map('n', 'F<Cr>', [[<cmd>Telescope lsp_workspace_symbols symbols=function,class<CR>]], opts)
+map('n', 'f<Cr>',     [[<cmd>Telescope lsp_document_symbols symbols=function,class,method<CR>]], opts)
+map('n', 'F<Cr>', [[<cmd>Telescope lsp_workspace_symbols symbols=function,class,method<CR>]], opts)
 map('n', 'T<Cr>', [[<cmd>Telescope lsp_workspace_symbols<CR>]], opts)
 if Installed('nvim-treesitter') then
   map('n', 't<Cr>', [[<cmd>Telescope treesitter<CR>]], opts)
