fe87fd30c25329d60a699296bb3d7c0b37ef857a
leoatchina <leoatchina@outlook.com>
2023-01-29 17:09:04 +0800

    add radian for iron.nvim

--- main/config/lua/iron.lua
+++ main/config/lua/iron.lua
@@ -43,3 +43,17 @@ iron.setup({
     italic = false
   }
 })
+if vim.fn.executable('radian') then
+  iron.core.add_repl_definitions {
+    r = {
+      radian = {
+        command = {"radian"}
+      }
+    },
+  }
+  iron.core.set_config {
+    preferred = {
+      r = "radian",
+    },
+  }
+end
