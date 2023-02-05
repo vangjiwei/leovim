6a5e758930b226480a7ed5b7aaf56956bd75f365
leoatchina <leoatchina@outlook.com>
2023-02-05 22:46:52 +0800

    update

--- clean.cmd
+++ clean.cmd
@@ -1,4 +1,4 @@
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
-call del "%HOME%\.leovim.d\swap\*.*"     /a /f /q
-call del "%HOME%\.leovim.d\backup\*.*"   /a /f /q
-call del "%HOME%\.leovim.d\shada.main.*" /a /f /q
+call del "%HOME%\.leovim.d\swap\*.*"   /a /f /q
+call del "%HOME%\.leovim.d\shada.*"    /a /f /q
+call del "%HOME%\.leovim.d\backup\*.*" /a /f /q
