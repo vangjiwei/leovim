0a9369807c5fd9a7663c37f65177953e2dfa8e95
leoatchina <leoatchina@outlook.com>
2023-02-23 12:03:24 +0800

    update nv.sh nvi.sh

--- scripts/nv.sh
+++ scripts/nv.sh
@@ -1,5 +1,5 @@
 if  [ -f "$HOME/.local/bin/nvim.appimage" ] && [ -x "$HOME/.local/bin/nvim.appimage" ];  then
-  NVIMCMD=$HOME/.local/bin/nvim.appimage
+  NVIMCMD="$HOME/.local/bin/nvim.appimage"
 elif [ -f "$HOME/.local/nvim-linux64/bin/nvim" ] && [ -x "$HOME/.local/nvim-linux64/bin/nvim" ]; then
   NVIMCMD="$HOME/.local/nvim-linux64/bin/nvim"
 elif [ -x "nvim" ]; then
