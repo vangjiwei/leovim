dc496f775fff17fd1073759e6df0e1ac8373dc9e
leoatchina <leoatchina@outlook.com>
2023-02-23 12:01:17 +0800

    update

--- scripts/nv.sh
+++ scripts/nv.sh
@@ -1,1 +1,11 @@
-nvim --cmd "let g:preset_group=['cmp']" "$@"
+if  [ -f "$HOME/.local/bin/nvim.appimage" ] && [ -x "$HOME/.local/bin/nvim.appimage" ];  then
+  NVIMCMD=$HOME/.local/bin/nvim.appimage
+elif [ -f "$HOME/.local/nvim-linux64/bin/nvim" ] && [ -x "$HOME/.local/nvim-linux64/bin/nvim" ]; then
+  NVIMCMD="$HOME/.local/nvim-linux64/bin/nvim"
+elif [ -x "nvim" ]; then
+  NVIMCMD="nvim"
+else
+  echo "nvim not executable"
+  return
+fi
+$NVIMCMD --cmd "let g:preset_group=['cmp']" "$@"
